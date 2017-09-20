57de5f58175f8a4b9d5134d5ddc1a185a804cf19
nicccccccccce <huxian901001@126.com>
2017-09-20 11:57:58 +0800

    Initial commit

---
+++ app/src/flavor/f2/com/sino/bridge/myapplication/TwoActivity.java
@@ -0,0 +1,19 @@
+package com.sino.bridge.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.eshi.bridge.pluggable.R;
+
+/**
+ * Created by user on 2017/1/10.
+ */
+
+public class TwoActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui_two);
+//        CopyJarUtil.releaseAssets();
+    }
+}
---
+++ app/src/main/com/eshi/bridge/pluggable/MainActivity.java
@@ -0,0 +1,17 @@
+package com.eshi.bridge.pluggable;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sino.bridge.myapplication.TwoActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, TwoActivity.class));
+    }
+}
